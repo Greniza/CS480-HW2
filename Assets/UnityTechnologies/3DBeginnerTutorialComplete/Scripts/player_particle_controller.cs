948d84bf45c5bf2e22933f70924dfe396653bef1
Greniza <61635418+Greniza@users.noreply.github.com>
2026-04-13 21:26:29 -0700

    particle scaling in proximity to next checkpoint

--- Assets/UnityTechnologies/3DBeginnerTutorialComplete/Scripts/player_particle_controller.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorialComplete/Scripts/player_particle_controller.cs
@@ -7,6 +7,7 @@ public class player_particle_controller : MonoBehaviour
 
 
     public GameObject current_checkpoint;
+    public double begin_shrink_distance;
 
     Vector3 target_position;
     ParticleSystem directed_particles;
@@ -21,10 +22,25 @@ public class player_particle_controller : MonoBehaviour
     void Update()
     {
         // make a vector that points to the next checkpoint
+        // LINEAR INTERPOLATION
         Vector3 pos_diff = current_checkpoint.transform.position - this.transform.position;
         // set it so it isn't pointing awkwardly upwards when close to a checkpoint
         pos_diff.y = 0;
         // face the position in question
         this.transform.rotation = Quaternion.LookRotation(pos_diff, Vector3.up);
+
+        // Now, we scale down the particle emission based on proximity to the next checkpoint
+        double distance = Vector3.Dot(pos_diff, pos_diff);
+        if (distance > begin_shrink_distance * begin_shrink_distance)
+        {
+            this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+        else
+        {
+            float shrink_scale = (float) (distance / (begin_shrink_distance * begin_shrink_distance));
+            shrink_scale = (shrink_scale + 1) / 2;
+            this.transform.localScale = new Vector3(shrink_scale, 1.0f, shrink_scale);
+        }
+
     }
 }
