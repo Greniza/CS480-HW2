7071a30c3d4bafa653691d6e26a1b55a04f5b843
Greniza <61635418+Greniza@users.noreply.github.com>
2026-04-13 21:44:19 -0700

    added a checkpoint get sound
    also moved checkpoint 3 so that it's in a safe area.

--- Assets/UnityTechnologies/3DBeginnerTutorialComplete/Scripts/checkpoint_system.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorialComplete/Scripts/checkpoint_system.cs
@@ -9,13 +9,26 @@ public class checkpoint_system : MonoBehaviour
 
     public GameObject particle_effect;
 
+    bool has_been_played;
+
+    void Start ()
+    {
+        has_been_played = false;
+    }
+
 
     void OnTriggerEnter (Collider other)
     {
         if (other.gameObject == player)
         {
             particle_effect.GetComponent<player_particle_controller>().current_checkpoint = next_checkpoint;
-            this.gameObject.SetActive(false);
+            // this.gameObject.SetActive(false);
+            if (!has_been_played)
+            {
+                GetComponent<AudioSource>().Play();
+                has_been_played = true;
+            }
+
         }
     }
 }
